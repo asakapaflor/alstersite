25c3df011d362532916647daf514644dbc673871
asakapaflor <joni_canale@yahoo.com>
2020-07-16 22:27:53 +0800

    deploy

---
+++ precache-manifest.bb2a3a7568c11274451aecc1c41d4f3a.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45faced3fd3e892e8d72",
+    "url": "/alstersite/css/app.5425f85b.css"
+  },
+  {
+    "revision": "b67283bd36b63884018d",
+    "url": "/alstersite/css/chunk-vendors.b081b468.css"
+  },
+  {
+    "revision": "2d44b10d34113797d676722f736154b9",
+    "url": "/alstersite/img/BasicComputerFamiliarization.2d44b10d.jpg"
+  },
+  {
+    "revision": "ad91eba6e3aa56aa674ba69f2fa8a56b",
+    "url": "/alstersite/img/BasicSafetyFamiliarization.ad91eba6.jpg"
+  },
+  {
+    "revision": "3e5be74059210e665609bd17db8d5fac",
+    "url": "/alstersite/img/ComputerBasedTraining.3e5be740.jpg"
+  },
+  {
+    "revision": "aad142b74f0507bd9671efcbeeef3744",
+    "url": "/alstersite/img/ComputerTroubleshooting.aad142b7.jpg"
+  },
+  {
+    "revision": "606af862bc8f252df9c717820dcb0311",
+    "url": "/alstersite/img/ControlEngineering.606af862.jpg"
+  },
+  {
+    "revision": "4aa38ee6b244b775323deb187f3537ec",
+    "url": "/alstersite/img/GlobalMaritime_DistressAndSafetySystem.4aa38ee6.jpg"
+  },
+  {
+    "revision": "ce15417841da687a256e2ce0f643248d",
+    "url": "/alstersite/img/HydraulicsPneumatic.ce154178.jpg"
+  },
+  {
+    "revision": "e3c0eb17d0049c843cf51603e5f4ef59",
+    "url": "/alstersite/img/IMG_0615-01.e3c0eb17.png"
+  },
+  {
+    "revision": "19b9f9dfb95388cd915b3c7a86203534",
+    "url": "/alstersite/img/InternationalSafetyManagement.19b9f9df.jpg"
+  },
+  {
+    "revision": "9cb70c6ff0c63dc60ef9469cb0666cfa",
+    "url": "/alstersite/img/MaritimeElectroTechnology.9cb70c6f.jpg"
+  },
+  {
+    "revision": "085edf10fc265cb4182796adbcef8f25",
+    "url": "/alstersite/img/New folder/mstile-150x150.png"
+  },
+  {
+    "revision": "5879a8ee1f04ed56cc1512eab30ad263",
+    "url": "/alstersite/img/ShipSecurityAwareness.5879a8ee.jpg"
+  },
+  {
+    "revision": "d1a58051c618021f7c61fa9c38a4215f",
+    "url": "/alstersite/img/TankerSafetyAndVetting.d1a58051.jpg"
+  },
+  {
+    "revision": "d0892513734eace14c1b1bcbc8ee16ce",
+    "url": "/alstersite/img/VoyagePlanning.d0892513.jpg"
+  },
+  {
+    "revision": "e9f7c6a14458ccf776be7d15cc5adeac",
+    "url": "/alstersite/img/accounting.e9f7c6a1.jpg"
+  },
+  {
+    "revision": "53d4b85dfc3785edb9aa8fa8229db657",
+    "url": "/alstersite/img/acctng-transparent.53d4b85d.png"
+  },
+  {
+    "revision": "81536b23c7db31ba4799d0410925c6bb",
+    "url": "/alstersite/img/alsterlogowhiteicon.81536b23.png"
+  },
+  {
+    "revision": "1a7afcb228cf84e8034cbe202bbc099f",
+    "url": "/alstersite/img/ceoMessage-1.1a7afcb2.jpg"
+  },
+  {
+    "revision": "8533b8adf031add4e08f88a1a2950423",
+    "url": "/alstersite/img/ceoMessage-11.8533b8ad.jpg"
+  },
+  {
+    "revision": "4c4bed36f2d65703ba43227588c67a53",
+    "url": "/alstersite/img/ceoMessage-12.4c4bed36.jpg"
+  },
+  {
+    "revision": "e25a4f019297bedb78c95bc1c40bf113",
+    "url": "/alstersite/img/ceoMessage-2.e25a4f01.jpg"
+  },
+  {
+    "revision": "48b459f3e44492c087f526140e898f71",
+    "url": "/alstersite/img/crewing.48b459f3.jpg"
+  },
+  {
+    "revision": "a4c9b86fb963007cecde8a9f263080ef",
+    "url": "/alstersite/img/facility1.a4c9b86f.jpg"
+  },
+  {
+    "revision": "8cc02d785afa9899d206a132ac9342b6",
+    "url": "/alstersite/img/facility10.8cc02d78.jpg"
+  },
+  {
+    "revision": "56c8da6b0a4a7ac5182c91eae41ee839",
+    "url": "/alstersite/img/facility11.56c8da6b.jpg"
+  },
+  {
+    "revision": "6001cddcacb108b7e7292cd340c086ba",
+    "url": "/alstersite/img/facility12.6001cddc.jpg"
+  },
+  {
+    "revision": "a6ec6f0742374f7677c678fa40380726",
+    "url": "/alstersite/img/facility13.a6ec6f07.jpg"
+  },
+  {
+    "revision": "ed5f4359776b4f6a3939e3c7fe6f75c7",
+    "url": "/alstersite/img/facility2.ed5f4359.jpg"
+  },
+  {
+    "revision": "7ef3afc08ba290985e38ab9487968b4a",
+    "url": "/alstersite/img/facility3.7ef3afc0.jpg"
+  },
+  {
+    "revision": "7b2a9239539a84d47b055fefa0e7e3e0",
+    "url": "/alstersite/img/facility4.7b2a9239.jpg"
+  },
+  {
+    "revision": "6e37f257ce10edb92404a5e76635fbae",
+    "url": "/alstersite/img/facility5.6e37f257.jpg"
+  },
+  {
+    "revision": "c52dc58f2ceafefd353b2c2d9d473050",
+    "url": "/alstersite/img/facility6.c52dc58f.jpg"
+  },
+  {
+    "revision": "c390051b92f1bd38fa7cb03c26553a23",
+    "url": "/alstersite/img/facility7.c390051b.jpg"
+  },
+  {
+    "revision": "67a43290498e367300fcf6fbd510b415",
+    "url": "/alstersite/img/facility8.67a43290.jpg"
+  },
+  {
+    "revision": "bdf01eebe898856bf7d14fa0bebe6251",
+    "url": "/alstersite/img/facility9.bdf01eeb.jpg"
+  },
+  {
+    "revision": "e0495805c739731b6eb47e234c13de74",
+    "url": "/alstersite/img/googleMap.e0495805.png"
+  },
+  {
+    "revision": "3d627517daa85d7393a9a3e7df292ecd",
+    "url": "/alstersite/img/homeSailor.3d627517.jpg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/alstersite/img/icons2/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/alstersite/img/icons2/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/alstersite/img/icons2/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/alstersite/img/icons2/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/alstersite/img/icons2/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/alstersite/img/icons2/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/alstersite/img/icons2/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/alstersite/img/icons2/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/alstersite/img/icons2/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/alstersite/img/icons2/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/alstersite/img/icons2/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/alstersite/img/icons2/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/alstersite/img/icons2/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/alstersite/img/icons2/mstile-150x150.png"
+  },
+  {
+    "revision": "6de16254d1b4193353e7015f9177535d",
+    "url": "/alstersite/img/icons2/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "2dca3288e968d3f45dcb49cbda4bf125",
+    "url": "/alstersite/img/isoLogo.2dca3288.png"
+  },
+  {
+    "revision": "e2f99444c3ffcac708d5d8a51fda4842",
+    "url": "/alstersite/img/it.e2f99444.jpg"
+  },
+  {
+    "revision": "4a464c15bc18809550d685e76bc71ac7",
+    "url": "/alstersite/img/managers.4a464c15.png"
+  },
+  {
+    "revision": "506e6fe14912d244132cbfa7d567e92b",
+    "url": "/alstersite/img/news1.506e6fe1.jpg"
+  },
+  {
+    "revision": "d6f888fd5589cb8baa533ff471c17682",
+    "url": "/alstersite/img/news1.d6f888fd.jpg"
+  },
+  {
+    "revision": "36753ced80ff1f067f65d08ef64350cc",
+    "url": "/alstersite/img/news10.36753ced.jpg"
+  },
+  {
+    "revision": "77388891e7ad4b664370873b91c5b06c",
+    "url": "/alstersite/img/news11.77388891.jpg"
+  },
+  {
+    "revision": "13a4f56527de8a1e62c0e6636feaeb64",
+    "url": "/alstersite/img/news12.13a4f565.jpg"
+  },
+  {
+    "revision": "6bf22fc50dee1f66d83e1767b73d38b4",
+    "url": "/alstersite/img/news13.6bf22fc5.jpg"
+  },
+  {
+    "revision": "e2f99444c3ffcac708d5d8a51fda4842",
+    "url": "/alstersite/img/news14.e2f99444.jpg"
+  },
+  {
+    "revision": "327fb19ce8c2e02a0276494d6e76c22d",
+    "url": "/alstersite/img/news15.327fb19c.jpg"
+  },
+  {
+    "revision": "cb015e35eec26a65f37b4a1cff863eee",
+    "url": "/alstersite/img/news16.cb015e35.jpg"
+  },
+  {
+    "revision": "5b94f518e9298751d5b8823b7c82ee84",
+    "url": "/alstersite/img/news2.5b94f518.jpg"
+  },
+  {
+    "revision": "ff5fd39a158eff9b987f9526795ff788",
+    "url": "/alstersite/img/news2.ff5fd39a.jpg"
+  },
+  {
+    "revision": "0bc130bf2783d9fcf0472bcee418297a",
+    "url": "/alstersite/img/news3.0bc130bf.jpg"
+  },
+  {
+    "revision": "b3fd2a32c9d22fb32756e01c67772741",
+    "url": "/alstersite/img/news3.b3fd2a32.jpg"
+  },
+  {
+    "revision": "7b54ee94028970758b221d1443c0edaa",
+    "url": "/alstersite/img/news4.7b54ee94.jpg"
+  },
+  {
+    "revision": "d4874805d34eb4d6e3adfbe80dc181ce",
+    "url": "/alstersite/img/news4.d4874805.jpg"
+  },
+  {
+    "revision": "45e91924b0e7e704aa9430d368eca000",
+    "url": "/alstersite/img/news5.45e91924.jpg"
+  },
+  {
+    "revision": "d20eab5a919c644530c6e7252ac2a121",
+    "url": "/alstersite/img/news5.d20eab5a.jpg"
+  },
+  {
+    "revision": "0d39943fa04fe19c234abaf0678996e5",
+    "url": "/alstersite/img/news6.0d39943f.jpg"
+  },
+  {
+    "revision": "33dc91046eae3d3b13c971503403fe0d",
+    "url": "/alstersite/img/news6.33dc9104.jpg"
+  },
+  {
+    "revision": "8907dddeb731a240110e70b5d1e4e209",
+    "url": "/alstersite/img/news7.8907ddde.jpg"
+  },
+  {
+    "revision": "c3fc8f2a3d34fa97835d128da0ce19e5",
+    "url": "/alstersite/img/news7.c3fc8f2a.jpg"
+  },
+  {
+    "revision": "2c66bedfbe225af8458715b3fd41a97b",
+    "url": "/alstersite/img/news8.2c66bedf.jpg"
+  },
+  {
+    "revision": "a4fe5e238bc78c09d2e06dc947d69edd",
+    "url": "/alstersite/img/news8.a4fe5e23.jpg"
+  },
+  {
+    "revision": "b1325661325fbb3e8719a30f4a9fea08",
+    "url": "/alstersite/img/news9.b1325661.jpg"
+  },
+  {
+    "revision": "bef7e4f7310d858438fa85b2d85c6890",
+    "url": "/alstersite/img/training.bef7e4f7.jpg"
+  },
+  {
+    "revision": "5af5115da95bd7093e7f553e1c20593c",
+    "url": "/alstersite/index.html"
+  },
+  {
+    "revision": "45faced3fd3e892e8d72",
+    "url": "/alstersite/js/app.19e81452.js"
+  },
+  {
+    "revision": "b67283bd36b63884018d",
+    "url": "/alstersite/js/chunk-vendors.f0d731da.js"
+  },
+  {
+    "revision": "f0e5f5fbcf04c1fa95c2a6cff9dc5a24",
+    "url": "/alstersite/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/alstersite/robots.txt"
+  }
+]);
